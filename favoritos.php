ae75a593755591cbf42d937b110bfd7882140b7a
BryanLLontop <bllontopc@unprg.edu.pe>
2023-09-06 16:38:23 -0500

    upload

---
+++ favoritos.php
@@ -0,0 +1,60 @@
+<?php include("template/header.php");?>
+
+		<!-- NAVIGATION -->
+		<nav id="navigation">
+			<!-- container -->
+			<div class="container">
+				<!-- responsive-nav -->
+				<div id="responsive-nav">
+					<!-- NAV -->
+					<ul class="main-nav nav navbar-nav">
+                        <li><a href="/index.php">Inicio</a></li>
+                        <li><a href="#">Tienda</a></li>
+						<li><a href="#">Categorias</a></li>
+						<li><a href="#">Ofertas</a></li>
+						<li><a href="/nosotros.php">Nosotros</a></li> 
+                        <li class="active"><a href="#">Favoritos</a></li>
+					</ul>
+					<!-- /NAV -->
+				</div>
+				<!-- /responsive-nav -->
+			</div>
+			<!-- /container -->
+		</nav>
+		<!-- /NAVIGATION -->
+
+		<!-- BREADCRUMB -->
+		<div id="breadcrumb" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+						<h3 class="breadcrumb-header">Favoritos</h3>
+						<ul class="breadcrumb-tree">
+							<li><a href="#">Inicio</a></li>
+							<li class="active">Favoritos</li>
+						</ul>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /BREADCRUMB -->
+
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+
+
+<?php include("template/footer.php");?>
